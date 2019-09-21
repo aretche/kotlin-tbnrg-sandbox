4d5e200cc4e3127337a381860fa1d46c98808b54
arellanog <arellanog@cent.gov.ar>
2019-09-21 12:36:59 -0300

    Challenge: hasSteed

---
+++ src/HasSteed.kt
@@ -0,0 +1,9 @@
+fun main() {
+    val playerName = "Estragon"
+    var experiencePoints = 5
+    var hasSteed = false
+    experiencePoints += 5
+    println(experiencePoints)
+    println(playerName)
+    println(hasSteed)
+}
