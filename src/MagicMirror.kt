e044f47040c43c5a13d769213444927a1feca7b8
arellanog <arellanog@cent.gov.ar>
2019-09-21 12:40:10 -0300

    Challenge: Magic Mirror

---
+++ src/MagicMirror.kt
@@ -0,0 +1,9 @@
+fun main() {
+    var playerName = "Estragon"
+    var experiencePoints = 5
+    experiencePoints += 5
+    println(experiencePoints)
+    println(playerName)
+    playerName = playerName.reversed()
+    println(playerName)
+}
