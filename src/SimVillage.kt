d68ba531daa3bdf6651ab434570ca9ac08871244
arellanog <arellanog@cent.gov.ar>
2019-09-23 23:04:17 -0300

    Listing 5.6 using type inference

--- src/SimVillage.kt
+++ src/SimVillage.kt
@@ -1,5 +1,5 @@
 fun main() {
-    val greetingFunction: (String, Int) -> String = { playerName, numBuildings ->
+    val greetingFunction = { playerName: String, numBuildings: Int ->
         val currentYear = 2018
         println("Adding $numBuildings house(s)")
         "Welcome to SimVillage, $playerName! (copyright $currentYear)"
